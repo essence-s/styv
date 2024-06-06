f20f8c030fe6c2b946dd2facfba852af1de688a2
styv <styv2301@gmail.com>
2024-06-06 13:56:32 -0500

    restructure components in home

--- src/components/layouts/home/partsHome/index.js
+++ src/components/layouts/home/partsHome/index.js
@@ -1,3 +1,3 @@
 export { default as Header } from './header/Header'
-export { default as Proyects } from './proyects/Proyects'
+export { default as Proyects } from './proyects/Proyects.astro'
 export { default as Me } from './me/Me'
--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,3 +1,3 @@
 // export {default as Navbar} from './navbar/Navbar'
 export { default as Footer } from './footer/Footer'
-export { default as Home } from './home/Home'
+export { default as Home } from './home/Home.astro'
