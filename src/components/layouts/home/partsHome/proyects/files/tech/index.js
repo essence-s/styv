080da4a0ba371a0cd91e573dcabfcb730c8fd6ce
styv <styv2301@gmail.com>
2024-10-04 12:06:24 -0500

    add proyect bot w

--- src/components/layouts/home/partsHome/proyects/files/tech/index.js
+++ src/components/layouts/home/partsHome/proyects/files/tech/index.js
@@ -1,8 +1,9 @@
-import css from './icon-css3.png'
-import html from './icon-html.png'
-import js from './icon-javascript.png'
-import ireact from './icon-react.png'
-import info from './info.png'
-import astro from './icon-astro.svg'
+import astro from './icon-astro.svg';
+import css from './icon-css3.png';
+import html from './icon-html.png';
+import js from './icon-javascript.png';
+import ireact from './icon-react.png';
+import info from './info.png';
+import nodejs from './node.svg';
 
-export { css, html, js, ireact, info, astro }
+export { astro, css, html, info, ireact, js, nodejs };
